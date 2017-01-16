7abe5b23930449e03840502041e56565c077d817
admin <admin@Kenneths-Retina-MacBook.local>
2017-01-16 12:27:06 +0000

    All commited

---
+++ rapport-chs-mobile/common/config/main-local.php
@@ -0,0 +1,52 @@
+<?php
+return [
+    'components' => [
+
+
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'sqlite:/Applications/XAMPP/xamppfiles/htdocs/rapport/forengineers/rapportforindependentengineers/chs/protected/data/chs.db', // SQLite
+
+
+            /*
+            'dsn' => 'mysql:host=localhost;dbname=findmyengineer',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            */
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'mail.laser.com',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
+                'username' => 'stalati',
+                'password' => '#rev1s1on!',
+                'port' => '25', // Port 25 is a very common port too
+                //'encryption' => 'none', // It is often used, check your provider or mail server specs
+            ],
+
+        ],
+
+        'clickatell' => [
+            'class' => 'albertborsos\clickatell\ClickatellHttp',
+            'username' => 'careys',
+            'password' => 'careys48',
+            'apiID' => '3601987',
+            'from' => 'careys', // optional parameter,
+            'mo' => 1 // optional parameter
+        ],
+
+
+
+
+
+
+
+    ],
+];
